01b6df85e262784b73a7264e2e529ab9274e0881
Ian McEwen <mian@cyverse.org>
2016-08-30 14:02:05 -0700

    Add :deploy-repositories for heuristomancer

--- project.clj
+++ project.clj
@@ -4,6 +4,8 @@
   :license {:name "BSD Standard License"
             :url "http://www.iplantcollaborative.org/sites/default/files/iPLANT-LICENSE.txt"}
   :profiles {:dev {:resource-paths ["test-data"]}}
+  :deploy-repositories [["releases" :clojars]
+                        ["snapshots" :clojars]]
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/tools.cli "0.3.2"]
                  [org.clojure/tools.logging "0.3.1"]
