bd6adce36623cae14bf270bbf0a97031fc693ac0
Ian McEwen <mian@cyverse.org>
2016-08-30 13:35:25 -0700

    Version 2.8.0 (+switch to org.cyverse)

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject org.iplantc/heuristomancer "5.2.8.0"
+(defproject org.cyverse/heuristomancer "2.8.0"
   :description "Clojure library for attempting to guess file types."
-  :url "https://github.com/iPlantCollaborativeOpenSource/DE"
+  :url "https://github.com/cyverse-de/heuristomancer"
   :license {:name "BSD Standard License"
             :url "http://www.iplantcollaborative.org/sites/default/files/iPLANT-LICENSE.txt"}
   :profiles {:dev {:resource-paths ["test-data"]}}
@@ -8,7 +8,6 @@
                  [org.clojure/tools.cli "0.3.2"]
                  [org.clojure/tools.logging "0.3.1"]
                  [instaparse "1.4.1"]]
-  :plugins [[org.iplantc/lein-iplant-cmdtar "5.2.8.0"]
-            [test2junit "1.1.3"]]
+  :plugins [[test2junit "1.1.3"]]
   :aot [heuristomancer.core]
   :main heuristomancer.core)
