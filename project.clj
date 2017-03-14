943048741a72e1acb47fd38af15a8884f5849030
Ian McEwen <mian@cyverse.org>
2017-03-14 12:00:04 -0700

    Version 2.8.3-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.cyverse/heuristomancer "2.8.2"
+(defproject org.cyverse/heuristomancer "2.8.3-SNAPSHOT"
   :description "Clojure library for attempting to guess file types."
   :url "https://github.com/cyverse-de/heuristomancer"
   :license {:name "BSD Standard License"
