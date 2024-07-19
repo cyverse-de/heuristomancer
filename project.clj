b75caf704dd75a9fe539cd5e8798a91aa761db4b
Sarah Roberts <sonoran.sarah@gmail.com>
2024-07-19 16:36:11 -0700

    updated the version to 2.8.8-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.cyverse/heuristomancer "2.8.7"
+(defproject org.cyverse/heuristomancer "2.8.8-SNAPSHOT"
   :description "Clojure library for attempting to guess file types."
   :url "https://github.com/cyverse-de/heuristomancer"
   :license {:name "BSD Standard License"
